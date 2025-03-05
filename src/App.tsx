f051676172c32b4e4c836e6571882e2e6f7b3576
josiah <josiahd.kitchin@gmail.com>
2025-03-05 01:26:48 -0800

    frontend up and working

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My React App!</h1>
+        <p>
+          This is a basic setup using TypeScript and React.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
