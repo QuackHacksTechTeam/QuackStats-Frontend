09456a620681c780464555119f5b4194d30592d1
josiah <josiahd.kitchin@gmail.com>
2025-03-19 22:14:02 -0700

    updated frontend menu button

--- src/data/chart_options.ts
+++ src/data/chart_options.ts
@@ -1,7 +1,8 @@
 
 
  export const CHART_OPTIONS = [
-    { label: 'Top 5 Committers', route: '/' },
+    { label: 'Home', route: '/'},
+    { label: 'Top 5 Committers', route: '/user-commits' },
     { label: 'Top 5 Repos with Most Commits', route: '/repo-commits' },
     { label: 'Top 5 Users with Most Lines of Code', route: '/user-loc'}
 ];
